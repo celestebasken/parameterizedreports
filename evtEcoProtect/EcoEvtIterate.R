2e439f02b5a41cd51a1e0bbae90e951ea98c1404
celestebasken <celeste.basken@gmail.com>
2021-05-11 12:16:11 -0400

    Celeste and Randy work

--- evtEcoProtect/EcoEvtIterate.R
+++ evtEcoProtect/EcoEvtIterate.R
@@ -8,10 +8,9 @@ ECO_NAME <- evt_combine$ECO_NAME
 
 reports <- tibble(
   input = "evtEcoProtect/EcoEvtReport.Rmd",
-  output_file = stringr::str_c("NEoutput/", ECO_NAME, "-test1.html"),
+  output_file = stringr::str_c("ouput2/", ECO_NAME, "-test1.html"),
   params = map(ECO_NAME, ~list(ECO_NAME = .))
 )
 
 reports %>%
   pwalk(render)
-
