01920c0b08dbc5389de558db49d94e45dd795316
celestebasken <celeste.basken@gmail.com>
2021-05-11 11:21:14 -0400

    Celeste work

---
+++ evtEcoProtect/EcoEvtIterate.R
@@ -0,0 +1,17 @@
+library(fivethirtyeight)
+library(tidyverse)
+library(rmarkdown)
+
+render(input = "evtEcoProtect/EcoEvtReport.Rmd")
+
+ECO_NAME <- evt_combine$ECO_NAME
+
+reports <- tibble(
+  input = "evtEcoProtect/EcoEvtReport.Rmd",
+  output_file = stringr::str_c("NEoutput/", ECO_NAME, "-test1.html"),
+  params = map(ECO_NAME, ~list(ECO_NAME = .))
+)
+
+reports %>%
+  pwalk(render)
+
